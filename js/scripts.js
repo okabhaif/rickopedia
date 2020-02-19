a315200329e58bd72b0e43285cbce093d4ac7346
okabhaif <okabhaif@users.noreply.github.com>
2020-02-19 22:55:40 +0000

    Updated
    Added styling + bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,18 @@
 var repository = (function() {
     var rickopedia = [];
     var apiUrl = 'https://rickandmortyapi.com/api/character/?page=1';
-    var $modalContainer = $('#modal-container');
+    var $modalContainer = $('.modal');
 
     function addListItem(character) {
-        var $rickopediaList = $('.rickopediaList')
+        var $rickopediaList = $('.rickopediaList');
 
         var $listItem = $('<li></li>');
         $rickopediaList.append($listItem);
 
-        var $button = $('<button class="button--li">' + character.name + '</button>');
+        var $button = $('<button class="btn btn-dark col-lg-8 col-sm button--li" data-toggle="modal" data-target="#modalContainer">' + character.name + '</button>');
         $listItem.append($button);
 
         ($button).on('click', function(event) {
-            $button.addClass('button-clicked');
             return showDetails(character);
         });
     }
@@ -66,34 +65,35 @@ var repository = (function() {
                 getCharacters(response.results);
 
 //scroll to bottom of screen event listener
-                var win = $(window);
+//updated code to target bottom of rickopedia div - used potato as var to make it easier to work with!
+                var potato = $('#potato');
                 var loading = false;
-                win.scroll(function() {
-                    if (win.height() + win.scrollTop()
-                                == $(document).height()) {
-                        if (!next) {
-                            next = url;
-                            console.log(url);
-                        }
 
-                        if (next && !loading) {
-                          //need to add a loading message
-                            $('#loading').show();
-                            return $.ajax(next, {
-                                    dataType: 'json'
-                                })
-                                .then(function(nextResponse) {
-                                    next = nextResponse.info.next;
-                                    var moreCharacters = getCharacters(nextResponse.results)
+                jQuery(function($) {
+            $('#potato').on('scroll', function() {
+                if(Math.ceil($(this).scrollTop()) + $(this).innerHeight() >= $(this)[0].scrollHeight) {
 
-                                    moreCharacters.forEach(function(character) {
-                                        addListItem(character);
-                                    });;
 
-                                });
-                        }
+                  if (next && !loading) {
+                    //need to add a loading message
+                      $('#loading').show();
+                      return $.ajax(next, {
+                              dataType: 'json'
+                          })
 
-                    }
+                          .then(function(nextResponse)
+                           {
+                              next = nextResponse.info.next;
+                              var moreCharacters = getCharacters(nextResponse.results)
+
+                              moreCharacters.forEach(function(character) {
+                                  addListItem(character);
+                              });
+
+                          });
+                  }                }
+            })
+
                 });
 
             });
@@ -122,24 +122,19 @@ var repository = (function() {
     function showModal(item) {
         // Clear all existing modal content
         console.log(item);
-        ($modalContainer).empty();
-
-        var $modal = $('<div class="modal"></div>');
-        ($modalContainer).append($modal);
 
         // Add the new modal content
-        var $closeButton = $('<button class="modal-close">Close</button>');
-        ($modal).append($closeButton);
-        ($closeButton).on('click', hideModal);
 
+        var $title = $('.modal-title');
+        ($title).text(item.name);
+//main details
+        var $modal = $('.modal-body');
+        ($modal).empty();
+        console.log($modal);
 
-        var $title = $('<h1>' + item.name + '</h1>');
-        ($modal).append($title);
-
-        var $img = $('<img></img>');
+        var $img = $('<img class="characterImage img-fluid"></img>');
         $img.attr('src', item.imageUrl);
         ($modal).append($img);
-
 
         var $species = $('<p>Species: ' + item.species + '</p>');
         ($modal).append($species);
@@ -152,28 +147,8 @@ var repository = (function() {
 
         var $origin = $('<p>Origin: ' + item.origin + '</p>');
         ($modal).append($origin);
-        ($modalContainer).addClass('is-visible');
-
     }
 
-    function hideModal() {
-        ($modalContainer).removeClass('is-visible')
-    }
-
-    $(window).on('keydown', function(e) {
-        if (e.key === 'Escape') {
-            hideModal();
-        }
-    });
-
-    ($modalContainer).on('click', (e) => {
-        // Since this is also triggered when clicking INSIDE the modal container,
-        // We only want to close if the user clicks directly on the overlay
-        var target = e.target;
-        if (target === $modalContainer) {
-            hideModal();
-        }
-    })
 
     return {
         add: add,
@@ -182,15 +157,17 @@ var repository = (function() {
         loadList: loadList,
         loadDetails: loadDetails,
         showModal: showModal,
-        hideModal: hideModal,
     };
 })();
-
-var $rickopediaList = $('.rickopediaList');
 
 repository.loadList().then(function() {
     // Now the data is loaded!
     repository.getAll().forEach(function(character) {
         repository.addListItem(character);
     });
+});
+
+var $toTop = $('.toTop');
+$toTop.on('click', function(){
+  $('#potato').scrollTop(0);
 });
